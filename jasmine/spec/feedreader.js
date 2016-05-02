e1076fbec5647f8c1ce223c17af2a01d62bfd18a
LiquidSwordsman <ctaylor@liquidswords.ninja>
2016-05-02 11:46:59 -0700

    feat: Add unit tests

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -11,7 +11,7 @@
 $(function() {
     /* This is our first test suite - a test suite just contains
     * a related set of tests. This suite is all about the RSS
-    * feeds definitions, the allFeeds variable in our application.
+    * feeds definitions, the (allFeeds variable in our application.
     */
     describe('RSS Feeds', function() {
         /* This is our first test - it tests to make sure that the
@@ -26,47 +26,82 @@ $(function() {
             expect(allFeeds.length).not.toBe(0);
         });
 
+        it('have well formed URLs', function(){
+            allFeeds.forEach(function(feed){
+                expect(feed.url).toBeDefined();
+                expect(feed.url).toEqual(jasmine.any(String));
+                expect(feed.url).not.toBe('');
+            })
+        });
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
-         * and that the URL is not empty.
-         */
-
-
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a name defined
-         * and that the name is not empty.
-         */
+        it('have well formed names', function(){
+            allFeeds.forEach(function(feed){
+                expect(feed.name).toBeDefined();
+                expect(feed.name).toEqual(jasmine.any(String));
+                expect(feed.name).not.toBe('');
+            });
+        });
     });
 
 
-    /* TODO: Write a new test suite named "The menu" */
+    describe('The menu', function(){
+        var theMenu = $(document.body);
 
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
-         */
+        it('is hidden by default', function(){
+            expect(theMenu.hasClass('menu-hidden')).toBe(true);
+        });
 
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
+        it('toggles visibility when clicked', function(){
+            var menuIcon = $('.menu-icon-link');
+            menuIcon.trigger('click');
+            expect(theMenu.hasClass('menu-hidden')).toBe(false);
+            menuIcon.trigger('click');
+            expect(theMenu.hasClass('menu-hidden')).toBe(true);
+        })
+    });
 
-    /* TODO: Write a new test suite named "Initial Entries" */
-
+    describe('Initial Entries', function(){
         /* TODO: Write a test that ensures when the loadFeed
-         * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
-         */
+        * function is called and completes its work, there is at least
+        * a single .entry element within the .feed container.
+        * Remember, loadFeed() is asynchronous so this test will require
+        * the use of Jasmine's beforeEach and asynchronous done() function.
+        */
+        beforeEach(function(done) {
+            loadFeed(0, done);
+        });
 
-    /* TODO: Write a new test suite named "New Feed Selection"
+        it('has at least one story', function(done){
+            expect($('.feed').children().length).toBeGreaterThan(0);
+            done();
+        });
+    });
 
-        /* TODO: Write a test that ensures when a new feed is loaded
-         * by the loadFeed function that the content actually changes.
-         * Remember, loadFeed() is asynchronous.
-         */
+    describe('New Feed Selection', function() {
+
+    	var feedIndex = 0;
+    	var feeds = [];
+
+    	// save a copy of the link text for allFeeds[1] and allFeeds[2] in feeds[]
+    	beforeEach(function(done) {
+    		loadFeed(feedIndex++, function() {
+    			feeds.push( $('.feed .entry-link .entry')[0]);
+    			done();
+    		});
+    	});
+
+    	it('first feed is loaded', function(done) {
+    		expect($('.feed .entry-link .entry').length).toBeGreaterThan(0);
+    		done();
+    	});
+
+
+    	it('second is loaded with different content than the first', function(done) {
+    		expect(feeds[0].innerHTML).not.toBe(feeds[1].innerHTML);
+    		done();
+    		// return to the Udacity feed
+    		loadFeed(0);
+    	});
+
+    });
 }());
